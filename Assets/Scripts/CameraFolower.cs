22a0ff08ad586654f235b42f5023a996bddf4b03
veraokulo <chumarev.dima@gmail.com>
2020-11-30 21:12:34 +0200

    added new character controller and ability to jump

---
+++ Assets/Scripts/CameraFolower.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class CameraFolower : MonoBehaviour
+{
+    public GameObject player;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        var playerPosition = player.transform.position;
+        playerPosition.z = transform.position.z;
+        transform.position = playerPosition;
+    }
+}

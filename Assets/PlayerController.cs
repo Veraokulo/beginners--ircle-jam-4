9aa9411cb32e74043a42bede23bad0703be41934
veraokulo <chumarev.dima@gmail.com>
2020-11-30 19:28:29 +0200

    added new testing scene

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    
+    public float horizontal;
+    public float speed;
+    public bool jump;
+
+
+    public float vertical;
+    
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal")*speed;
+        //jump = Input.GetKeyDown("Jump");
+    }
+
+    private void FixedUpdate()
+    {
+        var direction = new Vector3(horizontal, 0, 0);
+        rb.AddForce(direction);
+    }
+}
